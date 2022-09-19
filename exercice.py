288595391930cd6e5042643f840991db475fe3c4
jeanneprovost <jeanneeprovost@gmail.com>
2022-09-19 11:34:38 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,28 +3,36 @@
 
 import random
 
+
 def get_first_part_of_name(name):
-	return ""
+    first_part = name[0].capitalize() + name[1:name.find('-')].lower()
+    return 'Bonjour ' + first_part
+
 
 def get_random_sentence(animals, adjectives, fruits):
-	return ""
+    animal = animals[random.randint(0, len(animals) - 1)]
+    adjective = adjectives[random.randint(0, len(adjectives) - 1)]
+    fruit = fruits[random.randint(0, len(fruits) - 1)]
+    return f"Aujourd'hui, j'ai vu un {animal} s'emparer d'un panier {adjective} plein de {fruit}."
+
 
 def encrypt(text, shift):
-	return ""
+    return ""
+
 
 def decrypt(encrypted_text, shift):
-	return ""
+    return ""
 
 
 if __name__ == "__main__":
-	parrot = "jEaN-MARC"
-	print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
+    parrot = "jEaN-MARC"
+    print(f"Pour {parrot}, on a '{get_first_part_of_name(parrot)}'.")
 
-	animals = ("chevreuil", "chien", "pigeon")
-	adjectives = ("rouge", "officiel", "lourd")
-	fruits = ("pommes", "kiwis", "mangue")
-	print(get_random_sentence(animals, adjectives, fruits))
-	
-	print(encrypt("ABC", 1))
-	print(encrypt("abc 123 XYZ", 3))
-	print(decrypt("DEF 123 ABC", 3))
+    animals = ("chevreuil", "chien", "pigeon")
+    adjectives = ("rouge", "officiel", "lourd")
+    fruits = ("pommes", "kiwis", "mangue")
+    print(get_random_sentence(animals, adjectives, fruits))
+
+    print(encrypt("ABC", 1))
+    print(encrypt("abc 123 XYZ", 3))
+    print(decrypt("DEF 123 ABC", 3))
